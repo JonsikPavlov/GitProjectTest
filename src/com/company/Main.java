51bb897d6fbbc7f48f6a2148b9d5904ef43aa712
JonsikPavlov <Jonsik1993@tut.by>
2022-01-26 19:33:53 +0300

    Add method sum(int, int)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(sum(5,6));
+
+    }
+    public static int sum(int a, int b){
+        return a+b;
     }
 }
